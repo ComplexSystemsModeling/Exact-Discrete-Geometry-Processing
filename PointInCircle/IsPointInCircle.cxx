a99e9bb7c3ac9e8e0e4e1ae9ca8142e82d1916a7
Alex. Gouaillard <agouaillard@gmail.com>
2011-05-19 19:34:14 +0800

    ENH: add skewchuck predicates. Updates tests and code.

--- PointInCircle/IsPointInCircle.cxx
+++ PointInCircle/IsPointInCircle.cxx
@@ -6,52 +6,83 @@
 #include <iostream> 
 #include <cstdlib>
 
+extern "C"
+{
+double incircle(double* pa, double* pb, double* pc, double* pd);
+}
+
 int main( int argc, char** argv )
 {
-  if( argc < 3 )
+  if( argc < 4 )
     {
-    std::cout << "usage: <exe> epsilon_x epsilon_y";
+    std::cout << "usage: <exe> epsilon_x epsilon_y IsExact";
     std::cout << std::endl;
     }
-
-  typedef itk::Matrix< double, 4,4 > MatrixType;
-  MatrixType M;
 
   char **stringPtr;
   double epsilon_x = std::strtod( argv[1], stringPtr );
   double epsilon_y = std::strtod( argv[2], stringPtr );
-  double ax = 0.0;
+  std::cout << "Epsilon_x: " << epsilon_x << std::endl;
+  std::cout << "Epsilon_y: " << epsilon_y << std::endl;
+
+  double ax = 1.0;
   double ay = 0.0;
   double bx = 1.0;
-  double by = 0.0;
-  double cx = 1.0;
+  double by = 1.0;
+  double cx = 0.0;
   double cy = 1.0;
-  double dx = 0.0 + epsilon_x;
-  double dy = 1.0 + epsilon_y;
+  double dx = epsilon_x;
+  double dy = epsilon_y;
+  std::cout << "dx: " << dx << std::endl;
+  std::cout << "dy: " << dy << std::endl;
 
-  M(0,0) = ax;
-  M(0,1) = ay;
-  M(0,2) = ax*ax+ay*ay;
-  M(0,3) = 1.0;
-  M(1,0) = bx;
-  M(1,1) = by;
-  M(1,2) = bx*bx+by*by;
-  M(1,3) = 1.0;
-  M(2,0) = cx;
-  M(2,1) = cy;
-  M(2,2) = cx*cx+cy*cy;
-  M(2,3) = 1.0;
-  M(3,0) = dx;
-  M(3,1) = dy;
-  M(3,2) = dx*dx+dy*dy;
-  M(3,3) = 1.0;
+  double det;
+
+  if( atoi( argv[3] ) == 0 )
+    { 
+
+    typedef itk::Matrix< double, 4,4 > MatrixType;
+    MatrixType M;
+
+    M(0,0) = ax;
+    M(0,1) = ay;
+    M(0,2) = ax*ax+ay*ay;
+    M(0,3) = 1.0;
+    M(1,0) = bx;
+    M(1,1) = by;
+    M(1,2) = bx*bx+by*by;
+    M(1,3) = 1.0;
+    M(2,0) = cx;
+    M(2,1) = cy;
+    M(2,2) = cx*cx+cy*cy;
+    M(2,3) = 1.0;
+    M(3,0) = dx;
+    M(3,1) = dy;
+    M(3,2) = dx*dx+dy*dy;
+    M(3,3) = 1.0;
  
-  std::cout << "M: " << std::endl;
-  std::cout << M << std::endl;
+    std::cout << "M: " << std::endl;
+    std::cout << M << std::endl;
 
-  double det = vnl_det( M.GetVnlMatrix() );
+    det = vnl_det( M.GetVnlMatrix() );
+    std::cout << "Det(M): " << det << std::endl;
 
-  std::cout << "Det(M): " << det << std::endl;
+    }
+  else
+    {
+    double * pa = new double[2];
+    double * pb = new double[2];
+    double * pc = new double[2];
+    double * pd = new double[2]; 
+    pa[0] = ax; pa[1] = ay;
+    pb[0] = bx; pb[1] = by;
+    pc[0] = cx; pc[1] = cy;
+    pd[0] = dx; pd[1] = dy;
 
-  return(det>0?0:1);
+    det = incircle( pa, pb, pc, pd );
+    std::cout << "Det(M): " << det << std::endl;
+    }
+
+  return(det>=0?0:1);
+
 }
